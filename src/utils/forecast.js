42aa4f0b39feed90f33e807cc54d264e79a5ab08
fahimh7 <fhhimel523@gmail.com>
2020-02-16 18:09:37 +0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/ddc6fc2ca5fcfc05d5e645007d358419/' + latitude +',' + longitude ;
+
+    request({url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Unable to conect to weather service!', undefined);
+        }else if(body.error){
+            callback('Unable to find location!', undefined)
+        } else{
+            callback(undefined, body.daily.data[0].summary +' It is currently ' + body.currently.temperature + ' degree out. There is a ' + body.currently.precipProbability + ' % chance of rain');
+        }
+    })
+}
+
+module.exports = forecast
